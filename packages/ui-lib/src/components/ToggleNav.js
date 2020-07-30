46e35321f9422c7c5354d5f932a6e31a33ce94b5
Eric Masiello <eric.j.masiello@gmail.com>
2020-07-30 17:07:40 -0400

    define the basic component structure

--- packages/ui-lib/src/components/ToggleNav.js
+++ packages/ui-lib/src/components/ToggleNav.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import styles from './ToggleNav.module.scss';
 
-// TODO:
-function ToggleNav() {
-  return <div className={styles.toggle}>Hello</div>;
+export function ToggleNav(props) {
+  const { children } = props;
+
+  return <nav>{children}</nav>;
 }
 
-export default ToggleNav;
+export function ToggleButton(props) {
+  const { children } = props;
+
+  return <button>{children}</button>;
+}
+
+export function ToggleList(props) {
+  const { children } = props;
+
+  return <ul>{children}</ul>;
+}
+
+export function ToggleItem(props) {
+  const { children } = props;
+
+  return <li>{children}</li>;
+}
+
+export function ToggleLink(props) {
+  const { children } = props;
+
+  return <a>{children}</a>;
+}
--- packages/ui-lib/src/components/ToggleNav.story.js
+++ packages/ui-lib/src/components/ToggleNav.story.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import ToggleNav from './ToggleNav';
+import { ToggleNav, ToggleButton, ToggleList, ToggleItem, ToggleLink } from './ToggleNav';
 
 storiesOf('ToggleNav', module)
   .addDecorator(withKnobs)
-  .add('default', () => <ToggleNav />);
+  .add('default', () => (
+    <ToggleNav>
+      <ToggleButton>Some txt</ToggleButton>
+      <ToggleList>
+        <ToggleItem>
+          <ToggleLink href="google.com">Google</ToggleLink>
+        </ToggleItem>
+        <ToggleItem>
+          <ToggleLink href="google.com">Google</ToggleLink>
+        </ToggleItem>
+        <ToggleItem>
+          <ToggleLink href="google.com">Google</ToggleLink>
+        </ToggleItem>
+        <ToggleItem>
+          <ToggleLink href="google.com">Google</ToggleLink>
+        </ToggleItem>
+        <ToggleItem>
+          <ToggleLink href="google.com">Google</ToggleLink>
+        </ToggleItem>
+      </ToggleList>
+    </ToggleNav>
+  ));
